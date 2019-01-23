e29967e282e0806c2edefec5a2d9f75e4407667f
T0060assadasdasdasd927D <T0060927@W304A-PC-05>
2019-01-23 15:05:33 +0000

    Updating RM

--- SharpRSA/Maths.cs
+++ SharpRSA/Maths.cs
@@ -7,20 +7,66 @@ namespace SharpRSA
 {
     public class Maths
     {
+        public static Random rand = new Random(Environment.TickCount);
+
         /// <summary>
         /// A Rabin Miller primality test which returns true or false.
         /// </summary>
         /// <param name="num">The number to check for being likely prime.</param>
         /// <returns></returns>
-        public bool RabinMillerTest(BigInteger num)
+        public static bool RabinMillerTest(ulong n)
         {
+            //Zero and one are not prime.
+            if (n<2)
+            {
+                return false;
+            } else if (n%2==0)
+            {
+                //No multiples of two.
+                return false;
+            }
+
+            //Finding n-1 = 2^k * m.
+            long k = 1;
+            long m = 1;
+            double previousResult = 0;
+            while (true)
+            {
+                double result = (n - 1) / (Math.Pow(2, k));
+                
+                //Checking for a decimal return, breaking if so.
+                if (result%1!=0)
+                {
+                    //Returning to previous K value, which provided an integer.
+                    k--;
+                    m = (long)previousResult;
+                    break;
+                }
+                
+                //Incrementing and setting variables for next loop.
+                previousResult = result;
+                k++;
+
+                //Checking if we should just return false due to the loop being too large.
+                if (k>9999999999999999)
+                {
+                    Console.WriteLine("Loop cancelled due to infinite looping of k value.");
+                    return false;
+                }
+            }
+
+            //Picking a random number between 1 and n-1.
+            byte[] randBytes = new byte[63];
+            rand.NextBytes(randBytes);
+            
+
             return true;
         }
 
         //An overload wrapper for the RabinMillerTest which accepts a byte array.
-        public bool RabinMillerTest(byte[] bytes)
+        public static bool RabinMillerTest(byte[] bytes)
         {
-            BigInteger b = new BigInteger(bytes);
+            ulong b = BitConverter.ToUInt64(bytes, 0);
             return RabinMillerTest(b);
         }
     }
--- SharpRSA/RSA.cs
+++ SharpRSA/RSA.cs
@@ -4,8 +4,7 @@ namespace SharpRSA
 {
     public class SharpRSA
     {
-        public Random rand = new Random();
-        public byte[] FindPrime(int bitlength)
+        public ulong FindPrime(int bitlength)
         {
             //Generating a random number of bit length half of the given parameter.
             bitlength = bitlength / 2;
@@ -16,17 +15,17 @@ namespace SharpRSA
 
             //Filling bytes with pseudorandom.
             byte[] randomBytes = new byte[bitlength / 8];
-            rand.NextBytes(randomBytes);
+            Maths.rand.NextBytes(randomBytes);
 
             //Setting the bottom bit and top two bits of the number.
             //This ensures the number is odd, and ensures the high bit of N is set when generating keys.
             Utils.SetBitInByte(0, ref randomBytes[7]);
 
             //Performing a Rabin-Miller primality test.
-            
+            bool isPrime = Maths.RabinMillerTest(randomBytes);
 
             //placeholder
-            return new byte[8];
+            return new ulong();
         }
     }
 }

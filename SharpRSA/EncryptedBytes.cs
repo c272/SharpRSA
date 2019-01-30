3752f54c25ca0810151e3ced45916b661548acb4
T0060assadasdasdasd927D <T0060927@W304A-PC-05>
2019-01-30 14:55:20 +0000

    Editing small comment.

--- SharpRSA/EncryptedBytes.cs
+++ SharpRSA/EncryptedBytes.cs
@@ -6,7 +6,7 @@ namespace SharpRSA
 {
     public class EncryptedBytes<T>
     {
-        //Holds an array of encrypted byte arrays, all 126 bytes long (final one has no padding).
+        //Holds an array of encrypted byte arrays, all keybytelength-2 bytes long (final one has no padding).
         public List<byte[]> bytes;
         public T type;
 

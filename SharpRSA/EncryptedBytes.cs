f0c223eb14efc3d76c65142b9a5628ab987b0b01
T0060assadasdasdasd927D <T0060927@W304A-PC-05>
2019-01-30 14:54:49 +0000

    Adding new class for encrypted bytes larger than keybytes-2.

---
+++ SharpRSA/EncryptedBytes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpRSA
+{
+    public class EncryptedBytes<T>
+    {
+        //Holds an array of encrypted byte arrays, all 126 bytes long (final one has no padding).
+        public List<byte[]> bytes;
+        public T type;
+
+        //Constructor.
+        public EncryptedBytes()
+        {
+
+        }
+    }
+}
